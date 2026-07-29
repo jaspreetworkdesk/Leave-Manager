556588fb7d5e08acfc4af5ba73006d1a4ddb817e
jaspreet singh <jaspreetworkdesk@gmail.com>
2026-07-29 12:51:44 +0000

    fixed issues

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "http://127.0.0.1:8000/api",
-
+  baseURL:"https://www.flashaiit.com/leave-api/api"
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
 const nextConfig = {
-  basePath,
+  basePath: "/leave-manager",
+  trailingSlash: true,
+
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
